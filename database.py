10acdbe2678b10219cb62b945efc06d36375efa7
rishisingh1406 <codewithshivam1@gmail.com>
2026-09-16 17:10:06 +0530

    project update

---
+++ database.py
@@ -0,0 +1,19 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set.")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
